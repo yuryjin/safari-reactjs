3519494bf1d33701bda04abc77e3c94cd1f6c7c4
aquinas <aquinas1776@gmail.com>
2021-02-12 13:47:57 +0300

    Initial

---
+++ build/precache-manifest.72bca6924997ff0e5a8e7336f420764d.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c72a28a3bff87b611177d69b5085ac7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc8eff974c5035c1ff2f",
+    "url": "./static/css/2.04b35cf3.chunk.css"
+  },
+  {
+    "revision": "9ae1fc390223cd87092a",
+    "url": "./static/css/main.824cfa6c.chunk.css"
+  },
+  {
+    "revision": "cc8eff974c5035c1ff2f",
+    "url": "./static/js/2.6ce99ed8.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "./static/js/2.6ce99ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae1fc390223cd87092a",
+    "url": "./static/js/main.40b634d2.chunk.js"
+  },
+  {
+    "revision": "f9d7c8edf62c1d664023",
+    "url": "./static/js/runtime-main.48dc21bc.js"
+  },
+  {
+    "revision": "2c97aa73ec4894f33e02320d9fb7a4b6",
+    "url": "./static/media/about_page.2c97aa73.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "./static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "./static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "2beb2bc542da68e85c60328993392b7b",
+    "url": "./static/media/bookpage-hero.2beb2bc5.jpg"
+  },
+  {
+    "revision": "6b98666365764420d0f655b031760546",
+    "url": "./static/media/botswana.6b986663.jpg"
+  },
+  {
+    "revision": "38933ae95f78451f83857aa2deff7ab2",
+    "url": "./static/media/contact_page.38933ae9.jpg"
+  },
+  {
+    "revision": "0f1a21d100f20ea1f33791f352469c58",
+    "url": "./static/media/contactpage-hero.0f1a21d1.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "./static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "./static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "./static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "./static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "./static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "./static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "./static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "./static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "./static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "./static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "./static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "./static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "./static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "./static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "./static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "d77b6b6da3f4df96040911ac238aed98",
+    "url": "./static/media/homepage-hero.d77b6b6d.jpg"
+  },
+  {
+    "revision": "62c53c71fe91f5b2c05e6a8059c93204",
+    "url": "./static/media/kenya.62c53c71.jpg"
+  },
+  {
+    "revision": "31eaad9f9bbf9e322a3e2406d2677caf",
+    "url": "./static/media/rwanda.31eaad9f.jpg"
+  },
+  {
+    "revision": "8f0c085eacb04f598d19798e3622e179",
+    "url": "./static/media/tanzania.8f0c085e.jpg"
+  },
+  {
+    "revision": "e9253aa6e2ed8cc1e865437782822e92",
+    "url": "./static/media/uganda.e9253aa6.jpg"
+  },
+  {
+    "revision": "d0227f3892a110206e0153b2dfdf296d",
+    "url": "./static/media/zimbabwe.d0227f38.jpg"
+  }
+]);
---
+++ build_safari/precache-manifest.7bb24a6a3731c4f3d5321979b2778a09.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8437d2c91ab5976da56c79373f53d80c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "44c49cc418f2f554eb4b",
+    "url": "./static/css/2.ba3a0938.chunk.css"
+  },
+  {
+    "revision": "9fa0e0d3abc8a18de4e8",
+    "url": "./static/css/main.b94cff74.chunk.css"
+  },
+  {
+    "revision": "44c49cc418f2f554eb4b",
+    "url": "./static/js/2.7ad9fd42.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "./static/js/2.7ad9fd42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa0e0d3abc8a18de4e8",
+    "url": "./static/js/main.0bc60446.chunk.js"
+  },
+  {
+    "revision": "e1e85f41e5225622fb18",
+    "url": "./static/js/runtime-main.28f975d6.js"
+  },
+  {
+    "revision": "2c97aa73ec4894f33e02320d9fb7a4b6",
+    "url": "./static/media/about_page.2c97aa73.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "./static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "./static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "b0fc2266eb4910f6f755d5f7f1dc7b6d",
+    "url": "./static/media/book.b0fc2266.jpg"
+  },
+  {
+    "revision": "6b98666365764420d0f655b031760546",
+    "url": "./static/media/botswana.6b986663.jpg"
+  },
+  {
+    "revision": "38933ae95f78451f83857aa2deff7ab2",
+    "url": "./static/media/contact_page.38933ae9.jpg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "./static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "./static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "./static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "./static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "./static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "./static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "./static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "./static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "./static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "./static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "./static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "./static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "./static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "./static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "./static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "62c53c71fe91f5b2c05e6a8059c93204",
+    "url": "./static/media/kenya.62c53c71.jpg"
+  },
+  {
+    "revision": "eaca18e48155e3d6575b5ae213380e1c",
+    "url": "./static/media/rhino_mainpage.eaca18e4.jpg"
+  },
+  {
+    "revision": "31eaad9f9bbf9e322a3e2406d2677caf",
+    "url": "./static/media/rwanda.31eaad9f.jpg"
+  },
+  {
+    "revision": "8f0c085eacb04f598d19798e3622e179",
+    "url": "./static/media/tanzania.8f0c085e.jpg"
+  },
+  {
+    "revision": "e9253aa6e2ed8cc1e865437782822e92",
+    "url": "./static/media/uganda.e9253aa6.jpg"
+  },
+  {
+    "revision": "d0227f3892a110206e0153b2dfdf296d",
+    "url": "./static/media/zimbabwe.d0227f38.jpg"
+  }
+]);
